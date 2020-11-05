0953061b27e497df152c95729f11d7080ccb509c
MrGeektron <U^dl5TQrBQT7L@!N3>
2020-11-04 17:19:16 -0700

    Added game finished test update

--- src/Game.java
+++ src/Game.java
@@ -10,36 +10,31 @@ public class Game {
         AI = new AI(board);
     }
 
-    public boolean isGameFinished() {
-        boolean finished = true;
+    public GameEndTuple isGameFinished() {
+        // Check for completed winning rows
+        for(GameSquare[] row : board.rows) {
+            if(row[0].equals(row[1]) && row[0].equals(row[2])) {
+                if(row[0].value == GameSquare.Value.X) {
+                   return new GameEndTuple(true, "X");
+                }
+                else if(row[0].value == GameSquare.Value.O){
+                    return new GameEndTuple(true, "O");
+                }
+            }
+        }
         // Check if board has no more blank squares
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board.board[i][j].value == GameSquare.Value.BLANK) {
-                    finished = false;
+                return new GameEndTuple(false, "");
                 }
             }
         }
-        if(finished) {
-            System.out.println("Game Tied");
-            return true;
-        }
-        // Check for completed winning rows
-        for(GameSquare[] row : board.rows) {
-            if(row[0].equals(row[1]) && row[0].equals(row[2])) {
-                finished = true;
-                if(row[0].value == GameSquare.Value.X) {
-                    System.out.println("Player Wins");
-                }
-                else if(row[0].value == GameSquare.Value.O){
-                    System.out.println("AI Wins");
-                }
-                break;
-            }
-        }
-        return finished;
+        // If there are no blank squares and no winner it is a tie
+        return new GameEndTuple(true, "Tie");
     }
 
+    //TODO Have controller use receive input
     public boolean receiveInput(int row, int column, String OX) {
             if (row > 3 || row < 1 || column > 3 || column < 1) {
                 return false;
